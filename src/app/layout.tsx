e7c408d884fe7af6b87822f20cdede716cd53d77
yuremono <5alviaofficinali50@gmail.com>
2026-03-14 17:04:35 +0900

    chore: ヘッドタグを追加し.gitignoreを更新

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,14 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <meta name="author" content="著作者名" />
+        <link rel="canonical" href="https://example.com/page" />
+        <meta property="og:title" content="ページタイトル" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://example.com/page" />
+        <meta property="og:image" content="https://example.com/ogp.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@アカウント名" />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       </head>
